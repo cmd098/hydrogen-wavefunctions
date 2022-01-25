4f7c063f94592ebe232d0a97301506a3b4ebee95
CMD098 <97360169+cmd098@users.noreply.github.com>
2022-01-24 22:32:42 -0600

    Solve probability density

--- wavefunction.py
+++ wavefunction.py
@@ -11,6 +11,7 @@ import numpy as np
 import scipy.special as sp
 
 
+# Rnl(r) normalized radial function
 def radial_function(n, l, r, a0):
     laguerre = sp.genlaguerre(n - l - 1, 2 * l + 1)
     p = 2 * r / (n * a0)
@@ -21,6 +22,7 @@ def radial_function(n, l, r, a0):
     ) * np.exp(-p / 2) * p ** l * laguerre(p)
 
 
+# Ylm(θ,φ) normalized angular function (spherical harmonic function)
 def angular_function(l, m, theta, phi):
     legendre = sp.lpmv(m, l, np.cos(theta))
 
@@ -28,3 +30,22 @@ def angular_function(l, m, theta, phi):
         (2 * l + 1) * math.factorial(l - np.abs(m))
         / (4 * np.pi * math.factorial(l + np.abs(m)))
     ) * legendre * np.real(np.exp(1.j * m * phi))
+
+
+n = 2
+l = 0
+m = 0
+a0 = 10
+
+x_points = np.linspace(-480, 480, 680)
+y_points = x_points
+x, y = np.meshgrid(x_points, y_points)
+
+psi = radial_function(
+    n, l, np.sqrt((x ** 2 + y ** 2)), a0
+) * angular_function(
+    l, m, np.arctan(x / (y + 1e-10)), 0
+)
+
+# |Ψ|^2 probability density
+prob_density = np.abs(psi) ** 2
