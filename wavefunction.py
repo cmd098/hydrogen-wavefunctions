52439e02415340f3f093613bbbda3b8c633bc56f
CMD098 <97360169+cmd098@users.noreply.github.com>
2022-01-24 22:35:10 -0600

    Plot electron probability density

--- wavefunction.py
+++ wavefunction.py
@@ -9,6 +9,7 @@
 import math
 import numpy as np
 import scipy.special as sp
+import matplotlib.pyplot as plt
 
 
 # Rnl(r) normalized radial function
@@ -32,20 +33,36 @@ def angular_function(l, m, theta, phi):
     ) * legendre * np.real(np.exp(1.j * m * phi))
 
 
-n = 2
-l = 0
-m = 0
-a0 = 10
+# probability-density plot
+def plot_wavefunction(n, l, m, a0):
+    x_points = np.linspace(-480, 480, 680)
+    y_points = x_points
+    x, y = np.meshgrid(x_points, y_points)
 
-x_points = np.linspace(-480, 480, 680)
-y_points = x_points
-x, y = np.meshgrid(x_points, y_points)
+    # Ψ(r,θ,φ) = R(r)Y(θ,φ)
+    psi = radial_function(
+        n, l, np.sqrt((x ** 2 + y ** 2)), a0
+    ) * angular_function(
+        l, m, np.arctan(x / (y + 1e-10)), 0
+    )
 
-psi = radial_function(
-    n, l, np.sqrt((x ** 2 + y ** 2)), a0
-) * angular_function(
-    l, m, np.arctan(x / (y + 1e-10)), 0
-)
+    # |Ψ|^2 probability density
+    prob_density = np.abs(psi) ** 2
 
-# |Ψ|^2 probability density
-prob_density = np.abs(psi) ** 2
+    plt.figure(figsize=(8, 6))
+    plt.title('Hydrogen wavefunction probability density', fontsize=15, pad=20)
+    plt.text(25, 50, f'{n, l, m}', fontsize=14, color='white')
+    plt.text(780, 25, '+', fontsize=18)
+    plt.text(780, 690, '−', fontsize=18)
+    plt.text(
+        790, 560, 'Electron probability distribution',
+        rotation='vertical', fontsize=14
+    )
+
+    prob_density = np.sqrt(prob_density)
+    plt.imshow(prob_density, cmap='inferno')
+    cbar = plt.colorbar()
+    cbar.set_ticks([])
+    plt.savefig(f'wavefunction{n,l,m}.png')
+    plt.show()
+
